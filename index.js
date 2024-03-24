e253c3b1c938633f61ebcdc18ec798786f4dbfd3
Eder Peixoto Marques <epeixoto85@gmail.com>
2024-03-23 21:23:18 -0300

    Implantacao

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 import { ModuloPv } from './src/ModuloPv.js';
 
-const moduloPv = new ModuloPv( 2, 1, 'R' );
-
-console.log( moduloPv.rotateX( 90 ) );
+module.exports = {
+    ModuloPv
+}
